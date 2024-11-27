6757e8bda74916bc7afc0e44d14285adb10de0e9
Yomi017 <thuang017@connect.hkust-gz.edu.cn>
2024-11-27 15:20:37 +0800

    11.27

--- first_semeter/project/store.sql
+++ first_semeter/project/store.sql
@@ -1,20 +1,5 @@
 CREATE DATABASE db_university;
 USE DATABASE db_university;
-CREATE TABLE enrollment (
-  StudentID INTEGER,
-  CourseID INTEGER,
-  Semester TEXT
-);
-INSERT INTO enrollment VALUES (2,204,'Spring 2024');
-INSERT INTO enrollment VALUES (3,414,'Spring 2023');
-CREATE TABLE course (
-  CourseID INTEGER,
-  CourseName TEXT,
-  Credits INTEGER
-);
-INSERT INTO course VALUES (101,'Mathematics',3);
-INSERT INTO course VALUES (102,'Physics',4);
-INSERT INTO course VALUES (103,'Chemistry',3);
 CREATE TABLE student (
   ID INTEGER,
   Name TEXT,
@@ -23,3 +8,18 @@ CREATE TABLE student (
 INSERT INTO student VALUES (2,'Ali',1);
 INSERT INTO student VALUES (3,'Sara',3.8);
 INSERT INTO student VALUES (4,'Nora',3.2);
+CREATE TABLE course (
+  CourseID INTEGER,
+  CourseName TEXT,
+  Credits INTEGER
+);
+INSERT INTO course VALUES (101,'Mathematics',3);
+INSERT INTO course VALUES (102,'Physics',4);
+INSERT INTO course VALUES (103,'Chemistry',3);
+CREATE TABLE enrollment (
+  StudentID INTEGER,
+  CourseID INTEGER,
+  Semester TEXT
+);
+INSERT INTO enrollment VALUES (2,410,'Spring 2024');
+INSERT INTO enrollment VALUES (3,1662,'Spring 2023');
